a73e47a76188dbbe3b09d50366ac98dbcdf9252a
bruce <brucec@hotmail.com>
2020-01-25 14:34:57 -0800

    add comment to location where control function is to be inserted

--- src/controller.cpp
+++ src/controller.cpp
@@ -5,33 +5,31 @@
 ros::Publisher sailRudderPub;
 
 //replace message type with alex's custom message
-void chatterCallback(const sailbot::SailRudderRad::ConstPtr  & msg)
+void chatterCallback(const sailbot::SailRudderRad::ConstPtr &msg)
 {
-  std::string a = "avbdsadsa";
-  ROS_INFO("sail angle heard %s\n", std::to_string(msg->sail_angle).c_str());
-  ROS_INFO("rudder angle heard %s\n", std::to_string(msg->rudder_angle).c_str());
-    
+    ROS_INFO("sail angle heard %s\n", std::to_string(msg->sail_angle).c_str());
+    ROS_INFO("rudder angle heard %s\n", std::to_string(msg->rudder_angle).c_str());
+
+    //insert controller function here to calculate next sail and rudder angle
     sailbot::SailRudderRad msg1;
     msg1.sail_angle = 2 + msg->sail_angle;
-    msg1.rudder_angle = 5+ msg->rudder_angle;
-
-    ROS_INFO("some stuff");
+    msg1.rudder_angle = 5 + msg->rudder_angle;
     sailRudderPub.publish(msg1);
 }
 
 int main(int argc, char **argv)
 {
 
-  ros::init(argc, argv, "listener");
+    ros::init(argc, argv, "listener");
 
-  ros::NodeHandle n;
+    ros::NodeHandle n;
 
-  sailRudderPub = n.advertise<sailbot::SailRudderRad>("sail_rudder_angle", 1000);
+    sailRudderPub = n.advertise<sailbot::SailRudderRad>("sail_rudder_angle", 1000);
 
-//replace topic name with alex's sensor topic
-  ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+    //replace topic name with alex's sensor topic
+    ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
 
-  ros::spin();
+    ros::spin();
 
-  return 0;
+    return 0;
 }
